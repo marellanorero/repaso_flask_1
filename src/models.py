0223c5b5cd213d9f8361b0d33f46810e79a8abd8
marellanorero <marellanonorero@gmail.com>
2022-07-27 22:25:49 -0400

    Creating relationship One-to-one

--- src/models.py
+++ src/models.py
@@ -1,4 +1,3 @@
-from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
@@ -9,6 +8,7 @@ class User(db.Model):
     lastname = db.Column(db.String(50))
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
+    profile = db.relationship('Profile', backref='user', uselist=False)
 
     def serialize(self):
         return {
@@ -17,7 +17,58 @@ class User(db.Model):
             "lastname": self.lastname,
             "email": self.email
         }
+
+    def serialize_with_profile(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "profile": self.profile.serialize()
+        }
     
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    bio = db.Column(db.Text, default="")
+    twitter = db.Column(db.String(100), default="")
+    facebook = db.Column(db.String(100), default="")
+    instagram = db.Column(db.String(100), default="")
+    linkedin = db.Column(db.String(100), default="")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "bio": self.bio,
+            "twitter": self.twitter,
+            "facebook": self.facebook,
+            "instagram": self.instagram,
+            "linkedin": self.linkedin
+        }
+
+    def serialize_with_user(self):
+        return {
+            "id": self.id,
+            "name": self.user.name + " " + self.user.lastname,
+            "bio": self.bio,
+            "twitter": self.twitter,
+            "facebook": self.facebook,
+            "instagram": self.instagram,
+            "linkedin": self.linkedin
+        }
+
     def save(self):
         db.session.add(self)
         db.session.commit()
